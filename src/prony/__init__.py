3d5878c99778360205dac0bcfac49b79eb2f728f
hrganayath <117270815+hrganayath@users.noreply.github.com>
2026-02-23 11:10:59 +0100

    Update __init__.py

--- src/prony/__init__.py
+++ src/prony/__init__.py
@@ -1,15 +1,16 @@
 """Prony stabilization package."""
 
 from .core import prony_method
-from .residual import compute_residual_error
-from .utils import match_estimates, frequency_error_generic, wrap_to_nyquist
+from .residual import compute_residual_error, ResidualMetrics
+from .utils import match_estimates, relative_exponent_error, wrap_to_nyquist
 from .data import generate_clean_data, generate_noisy_data
 
 __all__ = [
     "prony_method",
     "compute_residual_error",
+    "ResidualMetrics",
     "match_estimates",
-    "frequency_error_generic",
+    "relative_exponent_error",
     "wrap_to_nyquist",
     "generate_clean_data",
     "generate_noisy_data",
